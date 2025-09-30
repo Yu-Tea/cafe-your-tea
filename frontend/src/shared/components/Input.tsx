4652c70c5c48e268e530d43b8eb5d54a79c18129
Yu-Tea <y68tanaka@gmail.com>
2025-09-30 19:44:23 +0900

    fix:vercelデプロイエラー修正

--- frontend/src/shared/components/Input.tsx
+++ frontend/src/shared/components/Input.tsx
@@ -1,21 +1,21 @@
-import type { InputHTMLAttributes } from "react";
+// import type { InputHTMLAttributes } from "react";
 
-type InputProps = {
-	className?: string;
-	id: string;
-	placeholder?: string;
-} & InputHTMLAttributes<HTMLInputElement>;
+// type InputProps = {
+// 	className?: string;
+// 	id: string;
+// 	placeholder?: string;
+// } & InputHTMLAttributes<HTMLInputElement>;
 
-const Input = ({ className, id, placeholder, ...props }: InputProps) => {
-	return (
-		<input
-			type="text"
-			id={id}
-			placeholder={placeholder}
-			{...props}
-			className={`input input-primary w-full ${className}`}
-		/>
-	);
-};
+// const Input = ({ className, id, placeholder, ...props }: InputProps) => {
+// 	return (
+// 		<input
+// 			type="text"
+// 			id={id}
+// 			placeholder={placeholder}
+// 			{...props}
+// 			className={`input input-primary w-full ${className}`}
+// 		/>
+// 	);
+// };
 
-export default Input;
+// export default Input;
